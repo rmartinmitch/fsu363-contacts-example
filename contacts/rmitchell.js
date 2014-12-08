21b2ae26761c817d8e7216b4d5a8c2595c6cbafc
rmartinmitch <rmartinmitch@gmail.com>
2014-12-08 09:35:56 -0500

    dec8.14.935a
    add new js

---
+++ contacts/rmitchell.js
@@ -0,0 +1,8 @@
+ {
+    "FullName":"Rick Mitchell",
+	"LastName":"Mitchell",
+	"FirstName":"Rick",
+	"EmailAddress":"rmitchell@framingham.edu",
+	"OfficePhone":"508.123.9874",
+	"MobilePhone":"978.356.1234"
+  }
